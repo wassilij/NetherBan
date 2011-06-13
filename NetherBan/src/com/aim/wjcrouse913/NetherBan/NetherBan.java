ca8e755770af6c3f7e95aec1f4385cac1635e5aa
Walker <wjcrouse913@aim.com>
2011-06-12 22:14:26 -0400

    License - Source

--- NetherBan/src/com/aim/wjcrouse913/NetherBan/NetherBan.java
+++ NetherBan/src/com/aim/wjcrouse913/NetherBan/NetherBan.java
@@ -27,6 +27,12 @@ import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 import org.bukkit.plugin.Plugin;
 
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
 public class NetherBan extends JavaPlugin {
 	public static PermissionHandler permissionHandler;
 	public static String nethername;
